01fa4058d4eadc71c4617f8cff4f9b1fc1b104f2
PdaDmitry <pda.gazcomp@gmail.com>
2024-05-25 16:54:05 +0300

    api.js,  maxPages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,15 @@
-import axios from 'axios';
 import SearchBar from '../SearchBar/SearchBar';
-import { useEffect, useState } from 'react';
 import ImageGallery from '../ImageGallery/ImageGallery';
 import Loader from '../Loader/Loader';
-import { Toaster } from 'react-hot-toast';
-// import css from './App.module.css';
 import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import fetchPhotos from '../../api';
+
+import { useEffect, useState } from 'react';
+import { Toaster } from 'react-hot-toast';
+import { nanoid } from 'nanoid';
+
+// import css from './App.module.css';
 
 export default function App() {
   const [query, setQuery] = useState('');
@@ -14,15 +17,12 @@ export default function App() {
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-
-  const ACCESS_KEY = '6nKFTVbV1CJGv4nIC51WkGEbMVOjX878ZeUV0kluA-Y';
+  const [maxPages, setMaxPages] = useState(1);
 
   const searchImages = async newQuery => {
-    setQuery(newQuery);
+    setQuery(`${nanoid()}/${newQuery}`);
     setPage(1);
     setImages([]);
-    setLoading(false);
-    setError(false);
   };
 
   const handleLoadMore = () => {
@@ -33,26 +33,17 @@ export default function App() {
     if (query === '') {
       return;
     }
-
     async function fetchData() {
       try {
         setError(false);
         setLoading(true);
 
-        const response = await axios.get('https://api.unsplash.com/search/photos/', {
-          params: {
-            client_id: ACCESS_KEY,
-            query,
-            per_page: 12,
-            orientation: 'landscape',
-            page,
-          },
-        });
+        const { results, maxPages } = await fetchPhotos(query.split('/')[1], page);
+
         setImages(prevImages => {
-          return [...prevImages, ...response.data.results];
+          return [...prevImages, ...results];
         });
-        // console.log(response.data.results);
-        // console.log(response);
+        setMaxPages(maxPages);
       } catch (error) {
         setError(true);
         console.log(error);
@@ -70,7 +61,7 @@ export default function App() {
       {error && <ErrorMessage />}
       {images.length > 0 && <ImageGallery items={images} />}
       {loading && <Loader />}
-      {images.length > 0 && !loading && <LoadMoreBtn onClick={handleLoadMore} />}
+      {images.length > 0 && !loading && page < maxPages && <LoadMoreBtn onClick={handleLoadMore} />}
       <Toaster />
     </div>
   );
